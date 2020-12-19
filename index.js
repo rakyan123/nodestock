05403c471dd55fb91260368f482bff4af7bceac9
Vaibhav Rakyan <letscode2000@gmail.com>
2020-12-20 01:21:26 +0530

    Created procfile

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const exphbs  = require('express-handlebars');
 const request = require('request');
+const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 5000;
 
@@ -12,8 +13,8 @@ on the frontend and hence its very useful */
 //API Key pk_caaa4f0fbf954c0a80fab2fabe67e7d6 
 //Will output entire details of FB on the console
 //function call_api
-function call_api(finishedAPI){
-	request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_caaa4f0fbf954c0a80fab2fabe67e7d6', {json:true}, (err,res,body) => {
+function call_api(finishedAPI, ticker){
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_caaa4f0fbf954c0a80fab2fabe67e7d6', {json:true}, (err,res,body) => {
 	if(err) return console.log(err);
 	if(res.statusCode === 200) {
 		//console.log(body);
@@ -27,16 +28,27 @@ function call_api(finishedAPI){
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+//Middleware
+app.use(bodyParser.urlencoded({extended:false}));
 
-
-//Set Handlebar routes
+//Set Handlebar index GET routes
 app.get('/', function (req, res) {
 	call_api(function(doneAPI){
 		res.render('home', {
-    	stock: doneAPI,
+    	stock: doneAPI, // Prints out object Object on the screen
     });
 
-	});
+	}, 'fb');
+    
+});
+
+app.post('/', function (req, res) {
+	call_api(function(doneAPI){
+		res.render('home', {
+    	stock: doneAPI, // Prints out object Object on the screen
+    });
+
+	}, req.body.stock_ticker);
     
 });
 
